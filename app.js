37ed52a14354d7cf2810add1e4fa12e60323e34b
Mkzay <80535194+Mkzay@users.noreply.github.com>
2023-07-31 12:51:39 +0100

    Email Feature

--- app.js
+++ app.js
@@ -1,47 +1,42 @@
-// JavaScript
 document.addEventListener('DOMContentLoaded', () => {
-    const mail = document.querySelector('#mail');
-    const form = document.querySelector('#form');
-    const errorElement = document.querySelector('#error');
-    const submit = document.querySelector('#submit');
-    const dismiss = document.getElementById('dismiss');
-  
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-  
-      let messages = [];
-  
-      if (mail.value.trim() === '') {
-        messages.push('');
-      } else {
+  const mail = document.querySelector('#mail');
+  const form = document.querySelector('#form');
+  const errorElement = document.querySelector('#error');
+  const submit = document.querySelector('#submit');
+  const dismiss = document.getElementById('dismiss');
 
-        if (!emailRegex.test(mail.value)) {
-          messages.push('Valid email required');
-        }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let messages = [];
+
+    if (mail.value.trim() === '') {
+      messages.push('');
+    } else {
+
+      if (!emailRegex.test(mail.value)) {
+        messages.push('Valid email required');
       }
- 
-      if (messages.length > 0) {
-        mail.style.borderColor = 'hsl(354, 100%, 66%)';
-        errorElement.textContent = messages.join(', ');
-        errorElement.style.display = 'block';
-      } else {
+    }
 
-        location.href = 'success.html';
-
-        //alert('Form submitted successfully!');
-
-        form.submit();
-      }
-    });
-  
-    mail.addEventListener('input', () => {
-      errorElement.style.display = 'none';
-      errorElement.textContent = '';
-    });
+    if (messages.length > 0) {
+      mail.style.borderColor = 'hsl(354, 100%, 66%)';
+      errorElement.textContent = messages.join(', ');
+      errorElement.style.display = 'block';
+    } else {
+      const email = mail.value;
+      location.href = `success.html?email=${encodeURIComponent(email)}`;
+    }
   });
 
-dismiss.addEventListener('click', function(){
+  mail.addEventListener('input', () => {
+    errorElement.style.display = 'none';
+    errorElement.textContent = '';
+  });
+});
+
+dismiss.addEventListener('click', function () {
   location.href = 'index.html';
 });
